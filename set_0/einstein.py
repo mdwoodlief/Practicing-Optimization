76e069d2fe1d79194794b5014f2512c7d420ade7
Matthew Woodlief <mdwoodlief@gmail.com>
2025-03-21 12:05:25 -0400

    Update einstein.py
    Condensed operation to one line. Removed 2 unnecessary variables.

--- set_0/einstein.py
+++ set_0/einstein.py
@@ -1,11 +1,7 @@
 def main():
-    #Mass input
-    m=int(input("What is the mass of the object? "))
-    #C2 light variable
-    c2=300000000*300000000
-    #Calculation and output
-    ENERGY=(m*c2)
-    print(ENERGY)
+    
+    #Mass input from user -> output E=MC2
+    print(int(input("What is the mass of the object? "))*300000000*300000000)
 
-
-main()
+if __name__ == '__main__':
+    main()
